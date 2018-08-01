a10e71120889c1c28e3c1424fa413943716d6ca3
peryaudo <peryaudo@gmail.com>
2018-08-01 21:09:15 +0900

    Add view controllers

---
+++ Skim/FeedListController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedListController.swift
+//  Skim
+//
+//  Created by Tetsui Ohkubo on 2018/08/01.
+//  Copyright © 2018年 peryaudo. All rights reserved.
+//
+
+import Cocoa
+
+class FeedListController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
