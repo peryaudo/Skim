611d666abb195be696d77e19acac1164ad6f0b4b
peryaudo <peryaudo@gmail.com>
2018-08-01 22:38:23 +0900

    Add WindowController and connect reload button.

---
+++ Skim/WindowController.swift
@@ -0,0 +1,20 @@
+//
+//  WindowController.swift
+//  Skim
+//
+//  Created by Tetsui Ohkubo on 2018/08/01.
+//  Copyright © 2018年 peryaudo. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+    @IBAction func reloadAction(_ sender: Any) {
+    }
+}
