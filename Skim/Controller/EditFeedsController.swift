7b4056141ef5b85cb37b8b37803f7d156bb9c1f1
peryaudo <peryaudo@gmail.com>
2018-08-05 23:16:34 +0900

    Still broken but implemented folder binding to edit feeds sheet

--- Skim/Controller/EditFeedsController.swift
+++ Skim/Controller/EditFeedsController.swift
@@ -9,22 +9,40 @@
 import Cocoa
 
 class EditFeedsController: NSViewController {
-    @IBOutlet var arrayController: NSArrayController!
+    @IBOutlet var feedsController: NSArrayController!
+    @IBOutlet var folderController: NSArrayController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         guard let appDelegate = NSApplication.shared.delegate as? AppDelegate else {
             return
         }
-        arrayController.managedObjectContext = appDelegate.persistentContainer.viewContext
-        arrayController.fetch(nil)
+        appDelegate.persistentContainer.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        feedsController.managedObjectContext = appDelegate.persistentContainer.viewContext
+        feedsController.fetch(nil)
+        folderController.managedObjectContext = appDelegate.persistentContainer.viewContext
+        folderController.fetch(nil)
     }
     
     @IBAction func cancelClickAction(_ sender: Any) {
+        guard let appDelegate = NSApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        appDelegate.persistentContainer.viewContext.rollback()
         dismissViewController(self)
     }
 
     @IBAction func okClickAction(_ sender: Any) {
-        dismissViewController(self)
+        guard let appDelegate = NSApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        do {
+            try appDelegate.persistentContainer.viewContext.save()
+            dismissViewController(self)
+        } catch {
+            let alert = NSAlert()
+            alert.messageText = "Cannot save"
+            alert.runModal()
+        }
     }
 }
